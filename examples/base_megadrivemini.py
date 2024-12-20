cb32c7d18ab714075332dbbae55d46679e151178
ukkari6 <me3t0m@gmail.com>
2024-12-20 10:55:32 +0900

    メガドライブミニ対応

--- examples/base_megadrivemini.py
+++ examples/base_megadrivemini.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-#デモジオリモート　リモート機能用基本コード
+#デモジオリモート　リモート機能のメガドラミニモード用基本コード
 #FT232RLのドライバが必要です
 #このコードはXubuntu 22.04.5 LTSで書いています。
 #
@@ -8,7 +8,8 @@
 # demogeoRemote（デモジオリモート）
 #向けのリモート機能用基本コードです。
 #
-#このコードではA,B,C,D,スタート,セレクト,スティック操作がPythonから出来るようになっています。
+#このコードはネオジオミニモード用のコードです。
+#A,B,C,X,Y,Z,スタート,モード、十字キー操作がPythonから出来るようになっています。
 #
 #ジョイスティックを右に送り続けるコードのみ実装していますが、
 #キーボード入力やUSBパッドのボタンを読み取ってデモジオリモートに送るだけで
@@ -20,19 +21,21 @@
 import serial
 import time
 import sys
+import random
 
 
 
 # ボタンのビットマスク
 BUTTONS = {
-    'BUTTON_A': 0b00000001,  # button0の0ビット目
+    'BUTTON_A': 0b00000100,  # button0の0ビット目
     'BUTTON_B': 0b00000010,  # button0の1ビット目
-    'BUTTON_C': 0b00001000,  # button0の3ビット目
-    'BUTTON_D': 0b00010000,  # button0の4ビット目
-    'BUTTON_SELECT': 0b00000100,  # button1の2ビット目
-    'BUTTON_START': 0b00001000,  # button1の3ビット目
+    'BUTTON_C': 0b00100000,  # button0の3ビット目
+    'BUTTON_X': 0b00001000,
+    'BUTTON_Y': 0b00000001,
+    'BUTTON_Z': 0b00010000,
+    'BUTTON_START':0b00000010,  # button1の3ビット目
+    'BUTTON_MODE': 0b00000001,   # button1の3ビット目
 }
-
 
 
 #シリアルに送信するパケットの生成
@@ -40,7 +43,7 @@ def gen_packet(button0, button1, axis_x, axis_y):
     header1 = 0xa5
     header2 = 0x5a
     
-    data = [header1, header2, button0, button1, 0, axis_y, axis_x, 0, 0, 0, 0, 0]
+    data = [header1, header2, button0, button1, axis_y, axis_x, 0, 0, 0, 0, 0, 0]
     return bytes(data)
 
 
@@ -49,9 +52,11 @@ def gen_packet(button0, button1, axis_x, axis_y):
 #BUTTON_A
 #BUTTON_B
 #BUTTON_C
-#BUTTON_D
-#BUTTON_SELECT
+#BUTTON_X
+#BUTTON_Y
+#BUTTON_Z
 #BUTTON_START
+#BUTTON_MODE
 def set_button_state(button_name, state, button0, button1):
 
   button = BUTTONS.get(button_name)  # ボタンのビットマスクを取得
@@ -59,12 +64,12 @@ def set_button_state(button_name, state, button0, button1):
     print(f"Invalid button name: {button_name}")
     return
 
-  if button_name in ['BUTTON_A', 'BUTTON_B', 'BUTTON_C', 'BUTTON_D']:  # button0
+  if button_name in ['BUTTON_A', 'BUTTON_B', 'BUTTON_C', 'BUTTON_X', 'BUTTON_Y', 'BUTTON_Z', ]:  # button0
     if state == "ON":
         button0 |= button
     else:
         button0 &= ~button
-  elif button_name in ['BUTTON_SELECT', 'BUTTON_START']:  # button1
+  elif button_name in ['BUTTON_START', 'BUTTON_MODE']:  # button1
     if state == "ON":
         button1 |= button
     else:
@@ -75,15 +80,18 @@ def set_button_state(button_name, state, button0, button1):
 
 #軸の状態更新（デジタル）
 def update_axis_state_digital(state, axis_x, axis_y):
-
     if state == "UP":
-        axis_y = 0x00  # 上
+        axis_x = 0x00  # 上
+        axis_y = 0x7F
     elif state == "DOWN":
-        axis_y = 0xFF  # 下
+        axis_x = 0xFF  # 下
+        axis_y = 0x7F
     elif state == "LEFT":
-        axis_x = 0x00  # 左
+        axis_x = 0x7F
+        axis_y = 0x00  # 左
     elif state == "RIGHT":
-        axis_x = 0xff  # 右
+        axis_x = 0x7F
+        axis_y = 0xFF  # 右
     else:
         axis_x = 0x7F  # 中央
         axis_y = 0x7F  # 中央
@@ -91,14 +99,11 @@ def update_axis_state_digital(state, axis_x, axis_y):
     return axis_x, axis_y
 
 
-
 #シリアルに送信するパケットを生成し、シリアルポートにバイナリデータを書き込む
 def send_data(ser, button0, button1, axis_x, axis_y):
     data = gen_packet(button0, button1, axis_x, axis_y)
     ser.write(data)
     print(f"Sent: {data}")   #送信データの表示
-
-
 
 
 if __name__ == "__main__":
@@ -109,8 +114,8 @@ if __name__ == "__main__":
     #デモジオに送るボタン変数
     button0 = 0
     button1 = 0
-    axis_x = 0x7F #0=左,0x3F=中央,0x7F=右
-    axis_y = 0x7F #0=上,0x3F=中央,0x7F=下
+    axis_x = 0x7F #0=左,0x7F=中央,0xFF=右
+    axis_y = 0x7F #0=上,0x7F=中央,0xFF=下
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
 
       while True:
@@ -118,10 +123,12 @@ if __name__ == "__main__":
         #axis_x, axis_y = update_axis_state_digital("RIGHT", axis_x, axis_y)  #ジョイスティックの状態を更新
         #button0, button1 = set_button_state("BUTTON_SELECT", "OFF", button0, button1) #ボタンの状態を更新
 
-        axis_x, axis_y = update_axis_state_digital("LEFT", axis_x, axis_y)  #ジョイスティックの状態を更新
+        axis_x, axis_y = update_axis_state_digital("RIGHT", axis_x, axis_y)  #ジョイスティックの状態を更新
+        #button0, button1 = set_button_state("BUTTON_START", "ON", button0, button1) #ボタンの状態を更新
         send_data(ser, button0, button1, axis_x, axis_y)  #デモジオにボタン状態を送信
         time.sleep(0.1)
 
         axis_x, axis_y = update_axis_state_digital("CENTER", axis_x, axis_y)  #ジョイスティックの状態を更新
+        #button0, button1 = set_button_state("BUTTON_START", "OFF", button0, button1) #ボタンの状態を更新
         send_data(ser, button0, button1, axis_x, axis_y)  #デモジオにボタン状態を送信
         time.sleep(0.5)
--- examples/usb_gamepad_to_megadrivemini.py
+++ examples/usb_gamepad_to_megadrivemini.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-#デモジオリモート　USBコントローラー　→　ネオジオミニブリッジコード
+#デモジオリモート　USBコントローラー　→　メガドライブミニブリッジコード
 #FT232RLのドライバが必要です
 #USBコントローラーの読み取りにPygameを使用していますので、ライブラリのインストールが必要です。
 #pip3 install pygame
-#このコードはXubuntu 22.04.5 LTSで書いています。
+#このコードはXubuntu 24.04.5 LTSで書いています。
 #
 #これはネオジオミニに挿すだけでデモ機能を実現するUSBデバイス
 # demogeoRemote（デモジオリモート）
-#用のUSBコントローラー　→　ネオジオミニブリッジコードです。
-#USBコントローラーの入力をネオジオミニに橋渡し（ブリッジ）します。
+#用のUSBコントローラー　→　メガドラミニモード用ブリッジコードです。
+#USBコントローラーの入力をメガドラミニに橋渡し（ブリッジ）します。
 #
 #様々な種類のUSBコントローラーに対応出来るように、このコード起動時に
-#A,B,C,D,START,SELECT
+#A,B,C,X,Y,Z,START,MODE
 #の割り当てをするようにしています。
 #
 #フルスピードで動かすとCPUに負担が掛かるので、
@@ -23,7 +23,10 @@ import serial
 import time
 import sys
 import pygame
-from base_neogeo import set_button_state, update_axis_state_digital, send_data
+from base_megadrivemini import set_button_state, update_axis_state_digital, send_data
+
+
+NUM_BUTTONS = 8 #割り当てるボタン数
 
 
 
@@ -36,12 +39,14 @@ def buttons_config():
   print(" ")
   print(" ")
   print(" ")
-  print("***********************************************************************************")
-  print("***  割り当てたいボタンを　A　B　C　D　E　START　SELECT　の順番で押してください ***")
-  print("***********************************************************************************")
+  print("*********************************")
+  print("  割り当てたいボタンを ")
+  print("  A　B　C　X　Y　Z　START　MODE  ")
+  print("  の順番で押してください  ")
+  print("*********************************")
 
   # イベントループ
-  while len(buttons) < 6:
+  while len(buttons) < NUM_BUTTONS:
     for event in pygame.event.get():
       if event.type == pygame.JOYBUTTONDOWN:
         button_index = event.button  # 押されたボタンの番号
@@ -61,8 +66,8 @@ if __name__ == "__main__":
     #デモジオに送るボタン変数 button0 = 0
     button0 = 0
     button1 = 0
-    axis_x = 0x3F #0=左,0x3F=中央,0x7F=右
-    axis_y = 0x3F #0=上,0x3F=中央,0x7F=下
+    axis_x = 0x7F #0=左,0x7F=中央,0xFF=右
+    axis_y = 0x7F #0=上,0x7F=中央,0xFF=下
     
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
 
@@ -88,7 +93,7 @@ if __name__ == "__main__":
       num_hats = joystick.get_numhats()
 
       # 割り当てるボタン名
-      button_names = ["BUTTON_A", "BUTTON_B", "BUTTON_C", "BUTTON_D", "BUTTON_START", "BUTTON_SELECT"]
+      button_names = ["BUTTON_A", "BUTTON_B", "BUTTON_C", "BUTTON_X", "BUTTON_Y", "BUTTON_Z", "BUTTON_START", "BUTTON_MODE"]
 
       #ボタン割り当て設定
       buttons = buttons_config()
@@ -103,15 +108,30 @@ if __name__ == "__main__":
 
           #ジョイスティックの状態を更新
           if event.type == pygame.JOYAXISMOTION:
-            axis_x = int((joystick.get_axis(0) + 1.0 ) / 2.0 * 256)
             axis_y = int((joystick.get_axis(1) + 1.0 ) / 2.0 * 256)
+            axis_x = int((joystick.get_axis(0) + 1.0 ) / 2.0 * 256)
           #ジョイスティックの状態がハット操作に干渉するので削除
           #ハット操作をジョイスティック操作に反映させる
           #elif event.type == pygame.JOYHATMOTION:
-          #  x, y = joystick.get_hat(0)
+            #x,y = joystick.get_hat(0)
+            #print("x")
+            #print(x)
+            #print("y")
+            #print(y)
           #  y = -y  #ハットの上下1と-1がAXISと逆なので反転
-          #  axis_x = int((x + 1.0 ) / 2.0 * 128)  #ハットの-1,0,1を0,0x3F,0x7Fに変換
-          #  axis_y = int((y + 1.0 ) / 2.0 * 128)
+            #axis_y = int((x + 1.0 ) / 2.0 * 255)  #ハットの-1,0,1を0,0x7F,0xFFに変換
+            #converted_value = int((y + 1))
+            #axis_x = int((x + 1.0 ) / 2.0 * 255)
+            #y = -y
+            #axis_y = int((y + 1.0 ) / 2.0 * 255)
+
+            #print("converted_value")
+            #print(converted_value)
+            #print("axis_y")
+            #print(axis_y)
+            #print("axis_x")
+            #print(axis_x)
+
 
           #ボタンの状態を更新
           for i, button_name in enumerate(button_names):
@@ -120,6 +140,6 @@ if __name__ == "__main__":
             else:
               button0, button1 = set_button_state(button_name, "OFF", button0, button1)
 
-          send_data(ser, button0, button1, axis_x, axis_y)  #デモジオにボタン状態を送信
+          send_data(ser, button0, button1, axis_y, axis_x)  #デモジオにボタン状態を送信
           pygame.time.wait(1) #フルスピードで動かすとCPUに負担が掛かるためウェイト
 
